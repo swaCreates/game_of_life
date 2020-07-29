592ad55f7b1921bcf6136139fd9d63a7484b4d0a
Francois Chery <swa_isthecreator@Francoiss-MBP.fios-router.home>
2020-07-29 18:51:10 -0400

    added buttons / ability to toggle cells

--- game_of_life/src/components/DisplayGrid/Grid.js
+++ game_of_life/src/components/DisplayGrid/Grid.js
@@ -1,5 +1,7 @@
 import React, {useState} from 'react';
 
+import produce from 'immer';
+
 const numRows = 30 // how I want grid to look
 const numCols = 30
 
@@ -13,17 +15,35 @@ export default function Grid() {
         return rows
     });
 
+    const [simulation, setSimulation] = useState(false);
+
+    const run = () =>{
+        setSimulation(!simulation);
+    };
+
     return (
-        <div className='grid-container'>
-            {/* rendering grid on screen */}
-            {grid.map((rows, i) => 
-                rows.map((col, j) => 
-                <div 
-                key={`${i}-${j}`} 
-                style={{
-                    backgroundColor: grid[i][j] ? 'purple' : undefined}} className='grid'>
-                </div>)
-            )}
-        </div>
+        <>
+            <div className='grid-container'>
+                {/* rendering grid on screen */}
+                {grid.map((rows, i) => 
+                    rows.map((col, j) => 
+                    <div 
+                    key={`${i}-${j}`} 
+                    onClick = {() => {
+                        const newGrid = produce(grid, gridCopy => {
+                            gridCopy[i][j] = grid[i][j] ? 0 : 1; // if it's currently alive then make it dead (toggle)
+                        });
+                        setGrid(newGrid);
+                    }}
+                    style={{
+                        backgroundColor: grid[i][j] ? 'purple' : undefined}} className='cells'>
+                    </div>)
+                )}
+            </div>
+            <div className='control-panel'>
+                <button onClick={run} className='start'>{simulation ? 'stop' : 'start'}</button>
+                <button className='randomize'>randomize</button>
+            </div>
+        </>
     );
 };
