724033dbb6d3bce74b03dee1f6c58e3b37bf5696
Francois Chery <swa_isthecreator@Francoiss-MBP.fios-router.home>
2020-08-04 18:15:15 -0400

    created next feature

--- game_of_life/src/components/DisplayGrid/Grid.js
+++ game_of_life/src/components/DisplayGrid/Grid.js
@@ -114,7 +114,7 @@ export default function Grid() {
             });
         });
 
-        const timer = setTimeout(runSimulation, speed);
+        setTimeout(runSimulation, speed);
     }, []);
     //
 
@@ -127,21 +127,56 @@ export default function Grid() {
         };
     };
 
-    const size = (size) => {
-        switch (size){
-            case '1':
-               setNumRows(20)
-               setNumCols(10)
-            break;
-            case '2':
-                setNumRows(50)
-                setNumCols(30)
-                break;
-            default:
-                setNumRows(70)
-                setNumCols(50)
-        }
-        resetGrid();
+    const runSimulationTwo = useCallback(() => { // useEffect type of hook
+        if (!simulationRef.current) { // if no simulation return
+            // console.log('no simulation')
+            return;
+        };
+
+        // console.log('simulation')
+        setGeneration((orig) => {
+            return orig + 1
+        });
+
+        // simulate
+        setGrid(currGrid => {
+            return produce(currGrid, gridCopy => {
+                for (let i = 0; i < numRows; i++) {
+                    for (let j = 0; j < numCols; j++) {
+                        let neighbors = 0;
+                        operations.forEach(([x, y]) => { // for x and y axis
+                            let newI = i + x;
+                            let newJ = j + y;
+                            if (newI >= 0 && newI < numRows && newJ >= 0 && newJ < numCols) {
+                                // helps keeping from going out of bounds
+                                neighbors += currGrid[newI][newJ] // if we have a live cell add +1 to neighbors
+                            };
+                        });
+
+                        // currGrid[i][j] && gridCopy[i][j] === cell
+                        // it references the value of the cell either (0 or 1)
+
+                        if (neighbors < 2 || neighbors > 3) {
+                            // fewer than 2 or more than 3 cells die
+                            // references rule 1 and rule 3
+                            gridCopy[i][j] = 0; // cell dies
+                        } else if (currGrid[i][j] === 0 && neighbors === 3) {
+                            // references rule 4
+                            gridCopy[i][j] = 1; // cell comes alive
+                        };
+                    };
+                };
+            });
+        });
+    }, []);
+
+    const runNext = () => { // function to run sim when clicked on button below
+        setSimulation(!simulation);
+
+        if (!simulation) {
+            simulationRef.current = true; // since initial is false we must toggle to true
+            runSimulationTwo();
+        };
     };
 
     const colorArr = [
@@ -189,19 +224,7 @@ export default function Grid() {
                 <button onClick={run} className='start'>{simulation ? 'Stop' : 'Start'}</button>
                 <button onClick={randomGrid} className='randomize'>Randomize</button>
                 <button onClick={resetGrid} className='clear'>Clear</button>
-                {/* <button onClick={runSlow} className='slow'>Slow</button> */}
-                {/* <button onClick={runFast} className='fast'>Fast</button> */}
-                <ButtonToolbar>
-                    <DropdownButton
-                    title='gridSize'
-                    className='size'
-                    onSelect={handleSelect}
-                    >
-                        <Dropdown.Item onSelect={size} eventKey='1'>20x10</Dropdown.Item>
-                        <Dropdown.Item eventKey='2'>50x30</Dropdown.Item>
-                        <Dropdown.Item eventKey='3'>70x50</Dropdown.Item>
-                    </DropdownButton>
-                </ButtonToolbar>
+                <button onClick={runNext} className='next'>Next</button>
             </div>
         </>
     );
